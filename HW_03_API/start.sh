1df8df139bfdcefe1f3ca1119a25ffbce226614c
antik9 <illarionovanton@gmail.com>
2018-04-03 20:07:51 +0300

    Change start.sh

--- HW_03_API/start.sh
+++ HW_03_API/start.sh
@@ -2,4 +2,4 @@
 
 # Build new docker image and run it interactively with --rm flag
 docker build -t otus_hw_03 .
-docker run --rm -it otus_hw_03
+docker run --rm -it -p 8080:8080 --net=host otus_hw_03
